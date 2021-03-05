c4e46a4f9c1132621745db540cecd9312013c0dd
Ricardo <ricardo.aguirre@rappi.com>
2021-03-05 10:03:18 -0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import FileUpload from "./components/FileUpload"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container" style={{ width: "600px" }}>
+      <div className="my-3">
+        <h3>Cargo</h3>
+        <h4>File Upload</h4>
+      </div>
+
+      <FileUpload />
+    </div>)
 }
 
-export default App;
+export default App
+
